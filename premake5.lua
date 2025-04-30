f318e29231c3c79ec4b5c2637cc5bf93771869b1
Aaron Masembe <aaronmasembemujabikalibala@gmail.com>
2025-04-30 11:47:20 +0200

    More things done

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+project "GLFW"
+	location "SulfameEngine/vendor/GLFW"
+	kind "StaticLib"
+	language "C"
+	cdialect "C11" -- Changed from cppdialect to cdialect
+	staticruntime "On"
+
+	targetdir ("bin/" .. outdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/GLFW/**.h",
+		"src/**.c",
+		"src/**.h",
+	}
+
+	includedirs
+	{
+		"SulfameEngine/vendor/GLFW/include",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		defines
+		{
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+	filter "configurations:Debug"
+		defines { "SE_DEBUG" }
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "SE_RELEASE" }
+		runtime "Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines { "SE_DIST" }
+		runtime "Release"
+		optimize "On"
